cedea449f71440e3b76e02feea5d72c5040eb743
Twark <nthierry@yahoo.fr>
2018-03-22 23:43:29 +0100

    Ajout du menu dans l'interface

--- imports/ui/layouts/app-body.js
+++ imports/ui/layouts/app-body.js
@@ -8,10 +8,16 @@ import { T9n } from 'meteor/softwarerero:accounts-t9n';
 
 import './app-body.html';
 
+Template.App_body.onRendered(function(){
+	$('.ui.sidebar').sidebar({context:$('#context')}).sidebar('setting', 'transition', 'overlay');
+})
+
 Template.App_body.events({
- 
-  'click .js-logout'() {
-    Meteor.logout();
+	'click .js-logout'() {
+    	Meteor.logout();
+	},
+	'click #menu_icon'() {
+    	$('.ui.sidebar').sidebar('toggle');
 	},
  });
 
